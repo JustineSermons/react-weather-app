e68b0f049f3cb308614564d9be32df26c42531be
Justine Sermons <31023947+JustineSermons@users.noreply.github.com>
2023-12-29 07:05:17 -0500

    removed githublink h3 element

--- src/Github.js
+++ src/Github.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Github() {
   return (
     <div className="github">
-      <h3 class="githubLink" />
       <a
         href="https://github.com/JustineSermons/weather-app-project"
         target="_blank" rel="noreferrer"
