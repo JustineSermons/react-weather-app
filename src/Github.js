fe2e06d261f83ceaf42638e0c8a2ec3969b14e8c
Justine Sermons <31023947+JustineSermons@users.noreply.github.com>
2023-12-29 07:14:14 -0500

    changed link to repository to correct one

--- src/Github.js
+++ src/Github.js
@@ -4,7 +4,7 @@ export default function Github() {
   return (
     <div className="github">
       <a
-        href="https://github.com/JustineSermons/weather-app-project"
+        href="https://github.com/JustineSermons/react-weather-app"
         target="_blank" rel="noreferrer"
       >
         GitHub repository
